858db2e1e629749ba4ffa15ef08c5f2d33217bb2
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-05-15 12:27:13 +0530

    new

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -88,7 +88,7 @@ export default function Contact() {
         <div className="h-64 w-full bg-gray-300 rounded-lg mb-8">
           {/* You can embed a Google Map iframe or other map service here */}
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.180439392272!2d-122.08424908444149!3d37.42199997982584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb6b0bb4f8b63%3A0xd3b4e2bcb4c6596d!2sGoogleplex!5e0!3m2!1sen!2sus!4v1613580533322!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31144.175952423004!2d85.83170889999999!3d20.296059999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19c7c05d6e1db7%3A0x4190e3de632915c8!2sTrident%20Group%20of%20Institutions!5e0!3m2!1sen!2sin!4v1663601596228!5m2!1sen!2sin"
             width="100%"
             height="100%"
             frameBorder="0"
